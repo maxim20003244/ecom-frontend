ee6b121248430dc4e2494ec7c9dbbcf728cdb5f3
Maxim Rotar <maxim20003244@gmail.com>
2025-04-27 23:48:34 -0700

    Setting up InputField Component and Config Redux for Authentication

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,7 @@
+const initialState = {
+    user : null,
+    address: [],
+}
+export const authReducer = (state = initialState , action) => {
+    return state;
+}
--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -2,12 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./ProductReducer";
 import { errorReducer } from "./errorReducer";
 import {cartReducer} from "./cartReducer";
+import { authReducer } from "./authReducer";
 
 const cartItems = localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
      : [];
+const user = localStorage.getItem("auth")
+    ? JSON.parse(localStorage.getItem("auth"))
+     : [];
 
      const initialState = {
+         auth: {user: user},
          carts : {cart : cartItems},
 
         //carts: { cart: [] },
@@ -18,7 +23,8 @@ export const store  = configureStore({
     reducer: {
          products: productReducer,
          errors : errorReducer,
-         carts: cartReducer
+         carts: cartReducer,
+         auth: authReducer,
 
     },
     preloadedState: initialState
